92f4ae1054d205fcbf1f1dd3128b268a994fd72e
Akash Gupta <42429043+iseakash@users.noreply.github.com>
2025-03-02 10:56:04 +0000

    modified demo file

---
+++ mlflow/demo.py
@@ -0,0 +1,24 @@
+import os
+import mlflow
+import argparse
+
+def evaluate(param1, param2):
+    metric = param1**2 + param2**2
+    return metric
+
+def main(p1, p2):
+    with mlflow.start_run():
+        mlflow.log_param("param1", p1) # key, value
+        mlflow.log_param("param2", p2) # key, value
+        ## log metric
+        metric = evaluate(param1=p1, param2=p2)
+        mlflow.log_metric("someMetric", metric)
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--param1", "-p1", type=int, default=2)
+    args.add_argument("--param2", "-p2", type=int, default=5)
+    parsed_args = args.parse_args()
+
+    main(parsed_args.param1, parsed_args.param2)
+
